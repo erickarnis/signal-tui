3e1c26b78ce966ae85158f3c79b84d2b09d91be2
Unknown <ekarnis@outlook.com>
2017-02-04 01:37:44 -0500

    refactored to have more logical calling order and more reliable shutdown and exception catching
    https://www.ibm.com/developerworks/library/l-python6/

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ and ncurses, which can be installed on ubuntu/debian with this:
 
 	 sudo apt install libncurses5-dev libncursesw5-dev
 
-Warning: it appears that when you message someone from this, you lose the ability to message that person from the phone number you used. Use at own risk. Hopefully this is a bug and can be fixed in the future.
+Warning: it appears that when you message someone from this, you lose the ability to message that person from the phone number you used. Use at own risk. This will hopefully cleared up when I implement adding other devices.
--- signal-tui.py
+++ signal-tui.py
@@ -12,32 +12,31 @@
                \______/     
 
 By Eric Karnis
+This will be under gpl someday
 '''
-#!/usr/bin/env python
-import os
+#!/usr/bin/env python3
+import curses, traceback, os
 from os import system
-import curses
-
-#window dressing
-def get_param(prompt_string):
-    screen.clear()
-    screen.border(0)
-    screen.addstr(2, 2, prompt_string)
-    screen.refresh()
-    input = screen.getstr(10, 10, 60)
-    return input
 
 #helper functions
+def get_param(prompt_string):
+    stdscr.clear()
+    stdscr.border(0)
+    stdscr.addstr(2, 2, prompt_string)
+    stdscr.refresh()
+    input = stdscr.getstr(10, 10, 60)
+    return input
+
 def execute_cmd(cmd_string):
     system("clear")
     a = system(cmd_string)
-    print ""
+    print ("")
     if a == 0:
-         print "Command executed correctly"
+         print ("Command executed correctly")
     else:
-         print "Command terminated with error"
+         print ("Command terminated with error")
     raw_input("Press enter")
-    print ""
+    print("")
 
 def file_is_empty(path):
     return os.stat(path).st_size==0
@@ -48,15 +47,15 @@ with open('user_data.txt', 'r') as file:
 
 username = user_data[0]
 
-######################
-##Main functionality##
-######################
+########################
+##Signal functionality##
+########################
 
 #Registers the user with Whisper Systems. If it works, they will send a
 #verification number to the user's phone
 def register():
     user_data[0] = username
-    with open('stats.txt', 'w') as file:
+    with open('user_data.txt', 'w') as file:
         file.writelines( user_data )
     curses.endwin()
     execute_cmd("signal-cli -u " + username + " register")
@@ -82,35 +81,66 @@ def check_messages():
 ##Interface##
 #############
 
-x = 0
+def main(stdscr):
+    x = 0
+    
+    while x != ord('5'):
+        stdscr = curses.initscr()
+    
+        stdscr.clear()
+        stdscr.border(0)
+        stdscr.addstr(2, 2, "Please enter a number...")
+        stdscr.addstr(4, 4, "1 - Register your number")
+        stdscr.addstr(5, 4, "2 - Enter verification code")
+        stdscr.addstr(6, 4, "3 - Send a message")
+        stdscr.addstr(7, 4, "4 - Check messages")
+        stdscr.addstr(8, 4, "5 - Exit")
+        stdscr.refresh()
+    
+        x = stdscr.getch()
+    
+        if x == ord('1'):
+            global username
+            username = get_param("Enter your phone number with country code eg for canada +16477798192") 
+            register()
+        if x == ord('2'):
+            verification_number = get_param("Enter verification code that should have been texted to your phone")
+            verify(verification_number)
+        if x == ord('3'):
+            recipient = get_param("Enter recipient's phone number with country code eg for canada +16477798192") 
+            message = get_param("Enter message") 
+            send_message(recipient, message)
+        if x == ord('4'): 
+            check_messages()
 
-while x != ord('5'):
-    screen = curses.initscr()
+#Initialize and call main
+if __name__=='__main__':
+    try:
+        # Initialize curses
+        stdscr=curses.initscr()
+        # Turn off echoing of keys, and enter cbreak mode,
+        # where no buffering is performed on keyboard input
+        curses.noecho()
+        curses.cbreak()
+  
+        # In keypad mode, escape sequences for special keys
+        # (like the cursor keys) will be interpreted and
+        # a special value like curses.KEY_LEFT will be returned
+        stdscr.keypad(1)
+        # Enter the main loop
+        main(stdscr)                    
+        # Set everything back to normal
+        stdscr.keypad(0)
+        curses.echo()
+        curses.nocbreak()
+        # Terminate curses
+        curses.endwin()                 
 
-    screen.clear()
-    screen.border(0)
-    screen.addstr(2, 2, "Please enter a number...")
-    screen.addstr(4, 4, "1 - Register your number")
-    screen.addstr(5, 4, "2 - Enter verification code")
-    screen.addstr(6, 4, "3 - Send a message")
-    screen.addstr(7, 4, "4 - Check messages")
-    screen.addstr(8, 4, "5 - Exit")
-    screen.refresh()
-
-    x = screen.getch()
-
-    if x == ord('1'):
-        global username
-    	username = get_param("Enter your phone number with country code eg for canada +16477798192") 
-    	register()
-    if x == ord('2'):
-        verification_number = get_param("Enter verification code that should have been texted to your phone")
-    	verify(verification_number)
-    if x == ord('3'):
-        recipient = get_param("Enter recipient's phone number with country code eg for canada +16477798192") 
-    	message = get_param("Enter message") 
-    	send_message(recipient, message)
-    if x == ord('4'): 
-    	check_messages()
-
-curses.endwin()
+    #If something goes wrong, restore terminal and report exception
+    except:
+        stdscr.keypad(0)
+        curses.echo()
+        curses.nocbreak()
+        curses.endwin()
+        #Print the exception
+        traceback.print_exc()           
