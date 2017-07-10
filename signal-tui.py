fe40b0c01e8fac863b2b7b7f2ce305247a00b517
Unknown <ekarnis@outlook.com>
2017-07-10 00:26:38 +0000

    Implemented password screen and partially implemented adding messages

--- signal-tui.py
+++ signal-tui.py
@@ -19,6 +19,8 @@ import curses
 import traceback
 import os
 import string
+import math
+import time
 
 from curses.textpad import Textbox, rectangle
 from os import system
@@ -78,7 +80,7 @@ if user_data:
 
 # Registers the user with Whisper Systems. If it works, they will send a
 # verification number to the user's phone
-def register():
+def register_device():
     user_data[0] = username
     with open('user_data.txt', 'w') as file:
         file.writelines(user_data)
@@ -87,9 +89,8 @@ def register():
 
 
 # Send's user's verification number to Whisper Systems
-def verify(verification_number):
+def verify_code(verification_number):
     curses.endwin()
-    # not sure why username has to be cast to string here, but not in register
     execute_cmd(
         "signal-cli -u " + username + " verify " + str(verification_number)
     )
@@ -109,6 +110,59 @@ def send_message(recipient, message):
 def check_messages():
     curses.endwin()
     execute_cmd("signal-cli -u " + str(username) + " receive")
+
+
+#########################
+#### Password Screen ####
+#########################
+def open_password_screen(password_attempts):
+    stdscr.clear()
+
+    x = int(curses.COLS / 2 - 40)
+
+    screen.addstr(10, x, "          @@\                               @@\          @@\               @@\ ")
+    screen.addstr(11, x, "          \__|                              @@ |         @@ |              \__|")
+    screen.addstr(12, x, " @@@@@@@\ @@\  @@@@@@\  @@@@@@@\   @@@@@@\  @@ |       @@@@@@\   @@\   @@\ @@\ ")
+    screen.addstr(13, x, "@@  _____|@@ |@@  __@@\ @@  __@@\  \____@@\ @@ |@@@@@@\|_@@  _|  @@ |  @@ |@@ |")
+    screen.addstr(14, x, "\@@@@@@\  @@ |@@ /  @@ |@@ |  @@ | @@@@@@@ |@@ |\______\ @@ |    @@ |  @@ |@@ |")
+    screen.addstr(15, x, " \____@@\ @@ |@@ |  @@ |@@ |  @@ |@@  __@@ |@@ |         @@ |@@\ @@ |  @@ |@@ |")
+    screen.addstr(16, x, "@@@@@@@  |@@ |\@@@@@@@ |@@ |  @@ |\@@@@@@@ |@@ |         \@@@@  |\@@@@@@  |@@ |")
+    screen.addstr(17, x, "\_______/ \__| \____@@ |\__|  \__| \_______|\__|          \____/  \______/ \__|")
+    screen.addstr(18, x, "              @@\   @@ |")
+    screen.addstr(19, x, "              \@@@@@@  |                                         By Eric Karnis")
+    screen.addstr(20, x, "               \______/ ")
+
+    if password_attempts < 3:
+        screen.addstr(23, int(curses.COLS / 2 - 7), "Enter Password")
+    else:
+        screen.addstr(25, int(curses.COLS / 2 - 7), "Too Many Attempts")
+        screen.refresh()
+        time.sleep(5)
+        quit()
+
+    rectangle(stdscr, 24, int(curses.COLS / 2 - 31), 26, int(curses.COLS / 2 + 31))
+    screen.refresh()
+
+    # Get then clean up password
+    password = stdscr.getstr(25, int(curses.COLS / 2 - 30), 60)
+    password = str(password)[2:]
+    password = password[:-1]
+
+    if password:
+        screen.addstr(27, int(curses.COLS / 2 - 7), password)
+
+    if check_password(password):
+        open_messages_panel()
+    else:
+        password_attempts += 1
+        open_password_screen(password_attempts)
+
+# TODO implement password creation, hashing, and storing
+def check_password(password):
+    if password == "eric":
+        return True
+    else:
+        return False
 
 
 ####################
@@ -148,7 +202,10 @@ def open_messages_panel():
     screen.vline(3, int(curses.COLS/4), curses.ACS_VLINE, curses.COLS - 3)
     stdscr.addstr(int(curses.LINES*(3/4)) - 2,
                   int(curses.COLS/4) + 1,
-                  "Enter message and hit Ctrl-G to send")
+                  " I to enter edit mode ")
+    stdscr.addstr(int(curses.LINES*(3/4)) - 2,
+                  curses.COLS - 17,
+                  " Ctrl-G to send ")
     # rectangle(stdscr, 1,0, 7, 32)
     stdscr.refresh()
 
@@ -160,28 +217,44 @@ def activate_editing():
                             int(curses.COLS*(3/4)) - 2,
                             int(curses.LINES*(3/4)),
                             int(curses.COLS/4) + 1)
+    editwin.bkgdset(curses.A_STANDOUT)
     box = Textbox(editwin)
-
+    box.stripspaces = True
     # Let the user edit until Ctrl-G is struck.
     box.edit()
 
     # Get resulting contents
     message = box.gather()
-    stdscr.refresh()
-    #input = stdscr.getstr(10, 10, 60)
 
     if message:
         # send_message(recipient, input)
-        rectangle(stdscr,
-                  int(curses.LINES*(1/2)),
-                  int(curses.COLS*(1/2)),
-                  int(curses.LINES*(3/4)) - 3,
-                  curses.COLS - 3)
-        stdscr.addstr(int(curses.LINES*(1/2)) + 1,
-                      int(curses.COLS*(1/2)) + 1,
-                      message)
+        add_message(message)
 
     curses.curs_set(False)
+    stdscr.refresh()
+
+
+def add_message(message):
+    # TODO push other messages about the page when a new one arrives
+    # TODO have a linked list or something similar to store messages in a buffer
+    message_line_len = int(curses.COLS*(1/2) - 5)
+    message_line_num = int(math.ceil(len(message)/message_line_len))
+    for x in range(0, message_line_num):
+        start_line_index = x * message_line_len
+        end_line_index = (x + 1) * message_line_len - 1
+        line = message[start_line_index: end_line_index]
+        try:
+            stdscr.addstr(int(curses.LINES*(1/2)) + x + 1,
+                          int(curses.COLS*(1/2)) + 1,
+                          line, curses.A_STANDOUT)
+        except curses.error:
+            pass
+
+    rectangle(stdscr,
+              int(curses.LINES*(1/2)),
+              int(curses.COLS*(1/2)),
+              int(curses.LINES*(3/4)) - 3,
+              curses.COLS - 3)
 
 
 def open_contacts_panel():
@@ -198,17 +271,14 @@ def open_settings_panel():
 
 def main(stdscr):
 
-    # Frame the interface area at initial terminal size
-    # BUG right now it crashes on resize
     global screen
     screen = stdscr.subwin(curses.LINES - 1, curses.COLS - 1, 0, 0)
     screen.box()
     screen.refresh()
 
-    # Prepare the screen
-    draw_top_menu()
-    open_messages_panel()
-
+    # Prepare the login screen
+    password_attempts = 0
+    open_password_screen(password_attempts)
     curses.curs_set(False)
     x = 0
     while x != ord("e"):
